6372533d12071bb06dbf38497b78e1358f9c4ce2
Travis Briggs <audiodude@gmail.com>
2013-12-30 12:02:36 -0800

    Max length and validation for usernames, names, and application

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ActiveRecord::Base
   validates_associated :websites, message: 'One or more of the given websites was invalid'
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
-  validates :username, presence: true, uniqueness: true
-  validates :name, presence: true
-  validates :application, length: { minimum: 50 }
+  validates(
+      :username,
+      presence: true,
+      uniqueness: true,
+      length: { 
+        maximum: 20
+      }, format: {
+        with: /\A[a-zA-Z0-9_-]+\z/,
+        message: "only allows letters, numbers, and '_-'"
+  })
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :application, length: { minimum: 50, maximum: 1000 }
 end
